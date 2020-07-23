8815479303f9ac6ffb5d4cff2f906a445d60ad8f
Michael Dao <mike@turing.io>
2020-07-23 17:37:52 -0600

    Fixes hound complaints

--- spec/features/home_page/user_can_see_campaign_name_spec.rb
+++ spec/features/home_page/user_can_see_campaign_name_spec.rb
@@ -1,13 +1,14 @@
-require 'rails_helper'
+# frozen_string_literal: true
+require "rails_helper"
 
 RSpec.describe "home page", type: :feature do
   context "as a user" do
     it "can see the campaign name on the home page" do
-      campaign_1 = Campaign.create(name: "Turing West Marches", status: "active")
+      campaign = Campaign.create(name: "Turing West Marches", status: "active")
 
       visit root_path
 
-      expect(page).to have_content("Turing West Marches")
-    end 
+      expect(page).to have_content("campaign.name")
+    end
   end
 end
