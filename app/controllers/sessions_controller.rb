1ad04b15e44c791c364ff4822d8d63817f020b12
Michael Dao <mike@turing.io>
2020-07-23 23:44:55 -0600

    refactors session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
-      redirect_to '/'
+      redirect_to root_path
     else
       flash[:error] = "Sorry, your credentials are bad."
       render :new
