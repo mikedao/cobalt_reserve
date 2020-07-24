bb32557de3eeab6f72d43f768e19182c075b93f4
Michael Dao <mike@turing.io>
2020-07-23 22:21:05 -0600

    more formatting fixes

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -25,9 +25,8 @@ RSpec.describe "User registration form", type: :feature do
 
       click_on "Create User"
 
-      expect(page).to have_content ("Welcome, #{username}!")
+      expect(page).to have_content "Welcome, #{username}!"
       expect(current_path).to eq(root_path)
     end
   end
 end
-
