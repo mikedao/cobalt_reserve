79dc33e954a0b33208205b892d11a459a1946373
Michael Dao <mike@turing.io>
2020-07-24 09:20:08 -0600

    completed testing

--- spec/features/users/user_can_log_out_spec.rb
+++ spec/features/users/user_can_log_out_spec.rb
@@ -24,28 +24,10 @@ RSpec.describe "Logging In", type: :feature do
     expect(page).to have_link("Log Out")
     expect(page).to_not have_link("Register")
     expect(page).to_not have_link("I already have an account")
-  end
-
-  it "cannot log in with bad credentials" do
-    Campaign.create(name: "Turing West Marches", status: "active")
-
-    user = User.create(username: "funbucket13",
-                       password: "test",
-                       email: "bucket@example.com")
-
-    visit "/"
 
     click_on "Log In"
 
-    expect(current_path).to eq("/login")
-
-    fill_in :username, with: user.username
-    fill_in :password, with: "Clearly incorrect"
-
-    click_on "Log In"
-
-    expect(current_path).to eq("/login")
-
-    expect(page).to have_content("Sorry, your credentials are bad.")
+    expect(current_path).to eq root_path
+    expect(page).to have_content("You have successfully logged out.")
   end
 end
