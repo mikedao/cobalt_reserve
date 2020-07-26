0f355fe5593ed04e714c5fd2b2193ca8e789dd6e
Michael Dao <mike@turing.io>
2020-07-26 10:42:42 -0600

    fixes formatting issues in test

--- spec/features/monsters/visitor_can_see_all_monsters_spec.rb
+++ spec/features/monsters/visitor_can_see_all_monsters_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "As a visitor", type: :feature do
   context "when I visit the monsters index page" do
     it "displays all monsters" do
-      monster1 = Monster.create( name: "Imp")
-      monster2 = Monster.create( name: "Helmed Horror")
+      monster1 = Monster.create(name: "Imp")
+      monster2 = Monster.create(name: "Helmed Horror")
 
       visit monsters_path
 
