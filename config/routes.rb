7b05162ba79b65103b5baae48b14d5429810231b
Michael Dao <mike@turing.io>
2020-07-26 10:40:59 -0600

    Monster index functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  resources :monsters, only: [:index]
 end
