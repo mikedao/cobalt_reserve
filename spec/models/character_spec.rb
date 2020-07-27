b61c841c317a9f20860d8772d8ba350c8b7f1cae
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-27 15:04:20 -0600

    Add assertions to Character model test regarding item relationship

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Character, type: :model do
   describe "relationships" do
     it { should belong_to :campaign }
     it { should belong_to :user }
+    it { should have_many :item_characters }
+    it { should have_many(:items).through(:item_characters) }
   end
 end
