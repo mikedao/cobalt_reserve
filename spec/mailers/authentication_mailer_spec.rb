c0f28cccc3f60931b33d2dc59c88fce19585755f
ian douglas <ian.douglas@iandouglas.com>
2020-07-27 22:19:26 -0600

    changed passwordless system to username, closes #37

--- spec/mailers/authentication_mailer_spec.rb
+++ spec/mailers/authentication_mailer_spec.rb
@@ -1,16 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe AuthenticationMailer, type: :mailer do
   it 'builds an outgoing message for passwordless auth' do
     fake_uuid = 'abc-123'
     allow(SecureRandom).to receive(:uuid) { fake_uuid }
-    campaign = Campaign.create!(name: 'Turing West Marches', status: 'active')
-    user = User.create!(username: 'funbucket13',
-                         password: 'test',
-                         email: 'bucket@example.com')
-    character = Character.create!(user: user, campaign: campaign, name: 'Cormyn')
+    Campaign.create!(name: 'Turing West Marches', status: 'active')
+    user = User.create!(
+      username: 'funbucket13',
+      password: 'test',
+      email: 'bucket@example.com',
+      status: 'active'
+    )
 
-    email = AuthenticationMailer.with(character: character).send_login_email
+    email = AuthenticationMailer.with(user: user).send_login_email
 
     assert_emails 1 do
       email.deliver_now
@@ -18,19 +20,19 @@ RSpec.describe AuthenticationMailer, type: :mailer do
 
     assert_equal ['no-reply@cobalt-reserve.com'], email.from
     assert_equal ['bucket@example.com'], email.to
-    assert_equal "Action Required: The Cobalt Reserve is waiting for the return of #{character.name}", email.subject
+    assert_equal "Action Required: #{user.username}, the Cobalt Reserve awaits your return!", email.subject
 
     plaintext_body = email.text_part.body.to_s
     html_body = email.html_part.body.to_s
 
-    expect(plaintext_body).to have_content("Hail, #{character.name}. The Cobalt Reserve awaits your presence.")
+    expect(plaintext_body).to have_content("Hail, #{user.username}. The Cobalt Reserve awaits your return.")
     expect(plaintext_body).to have_content("http://cobalt-reserve.com/auth/#{fake_uuid}")
-    expect(plaintext_body).to have_content("This link expires in 10 minutes.")
+    expect(plaintext_body).to have_content('This link expires in 10 minutes.')
 
-    expect(html_body).to have_content("Hail, #{character.name}. The Cobalt Reserve awaits your presence.")
-    expect(html_body).to have_content("Click here to return to the Cobalt Reserve.")
+    expect(html_body).to have_content("Hail, #{user.username}. The Cobalt Reserve awaits your return.")
+    expect(html_body).to have_content('Click here to log in to the Cobalt Reserve.')
     expect(html_body).to have_selector(:css, "a[href=\"http://cobalt-reserve.com/auth/#{fake_uuid}\"]")
-    expect(html_body).to have_content("This link expires in 10 minutes.")
+    expect(html_body).to have_content('This link expires in 10 minutes.')
 
     # how many actual emails were caught by Rails 6 test environment?
     assert_equal ActionMailer::Base.deliveries.count, 1
