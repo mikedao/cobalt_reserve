7b4c9103d78d648d82c8a61df3dd9231ec3b39f6
ian douglas <ian.douglas@iandouglas.com>
2020-07-28 14:23:08 -0600

    closes #41, closes #44

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_035547) do
+ActiveRecord::Schema.define(version: 2020_07_28_175509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_035547) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
-    t.string "race"
+    t.string "species"
     t.integer "level"
     t.string "character_class"
     t.datetime "created_at", precision: 6, null: false
--- spec/factories/character_factory.rb
+++ spec/factories/character_factory.rb
@@ -3,10 +3,11 @@ require 'faker'
 FactoryBot.define do
   factory :character do
     name { Faker::Games::WorldOfWarcraft.unique.hero }
-    race { Faker::Games::DnD.species }
+    species { Faker::Games::DnD.species }
     level { Faker::Number.within(range: 1..10) }
     character_class { Faker::Games::DnD.klass }
     campaign
     user
+    active { true }
   end
 end
