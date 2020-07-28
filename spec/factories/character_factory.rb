fa666c734cac6ca65ae89c4af3d6af5c2124a3a1
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-27 22:21:05 -0600

    Fix duplicate character name problem in test data

--- spec/factories/character_factory.rb
+++ spec/factories/character_factory.rb
@@ -2,7 +2,7 @@ require "faker"
 
 FactoryBot.define do
   factory :character do
-    name { Faker::Games::WorldOfWarcraft.hero }
+    name { Faker::Games::WorldOfWarcraft.unique.hero }
     race { Faker::Games::DnD.species } 
     level { Faker::Number.within(range: 1..10) }
     character_class { Faker::Games::DnD.klass }
