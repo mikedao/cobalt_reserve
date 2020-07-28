870d436e2766ad486794a0298966fb60922e785f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-27 21:37:53 -0600

    Require Faker in helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,8 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+require 'faker'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
