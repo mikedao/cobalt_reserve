e5b7ab8a9ca8e159eb6834d84abce9ff03e8d384
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-27 21:38:53 -0600

    Add assertions and test data to feature test

--- spec/features/characters/user_can_see_characters_spec.rb
+++ spec/features/characters/user_can_see_characters_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe "character index", type: :feature do
   before(:each) do
-    active_campaign = create(:campaign)
-    old_campaign = create(:campaign, status: 'inactive')
-    newer_campaiagn = create(:campaign)
+    @active_campaign = create(:campaign)
+    @old_campaign = create(:campaign, status: 'inactive')
+    @newer_campaign = create(:campaign)
 
-    character_1 = create(:character, campaign: active_campaign) # expected
-    character_2 = create(:character, campaign: active_campaign) # expected
-    character_3 = create(:character, campaign: old_campaign)    # not expected
-    character_4 = create(:character, campaign: newer_campaign)  # not expected
+    @character_1 = create(:character, campaign: @active_campaign) # expected
+    @character_2 = create(:character, campaign: @active_campaign) # expected
+    @character_3 = create(:character, campaign: @old_campaign)    # not expected
+    @character_4 = create(:character, campaign: @newer_campaign)  # not expected
   end
 
   context "as a visitor" do
@@ -19,10 +19,10 @@ RSpec.describe "character index", type: :feature do
       end
 
       it "I see a list of all the characters for the active campaign" do
-        expect(page).to have_content(character_1.name)
-        expect(page).to have_content(character_2.name)
-        expect(page).not_to have_content(character_3.name)
-        expect(page).not_to have_content(character_4.name)
+        expect(page).to have_content(@character_1.name)
+        expect(page).to have_content(@character_2.name)
+        expect(page).not_to have_content(@character_3.name)
+        expect(page).not_to have_content(@character_4.name)
       end
     end
   end
