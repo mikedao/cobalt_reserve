805a3fbe1dab9fa15f66d7d3ba21793113d333bd
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-27 21:17:06 -0600

    Create test for character index feature

---
+++ spec/features/characters/user_can_see_characters_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "character index", type: :feature do
+  before(:each) do
+    active_campaign = create(:campaign)
+    old_campaign = create(:campaign, status: 'inactive')
+    newer_campaiagn = create(:campaign)
+
+    character_1 = create(:character, campaign: active_campaign) # expected
+    character_2 = create(:character, campaign: active_campaign) # expected
+    character_3 = create(:character, campaign: old_campaign)    # not expected
+    character_4 = create(:character, campaign: newer_campaign)  # not expected
+  end
+
+  context "as a visitor" do
+    context "when I visit /characters" do
+      before(:each) do
+        visit "/characters"
+      end
+
+      it "I see a list of all the characters for the active campaign" do
+        expect(page).to have_content(character_1.name)
+        expect(page).to have_content(character_2.name)
+        expect(page).not_to have_content(character_3.name)
+        expect(page).not_to have_content(character_4.name)
+      end
+    end
+  end
+end
