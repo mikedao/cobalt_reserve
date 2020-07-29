7a94c57c2ea3a660dcf8deef80d027b1d235fdee
Hashrocket Workstation <dev@hashrocket.com>
2020-07-28 20:41:30 -0400

    Add password confirmation

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe 'User registration form', type: :feature do
 
       fill_in :username, with: username
       fill_in :password, with: password
+      fill_in :password_confirmation, with: password
       fill_in :first_name, with: first_name
       fill_in :last_name, with: last_name
       fill_in :email, with: email
@@ -26,6 +27,34 @@ RSpec.describe 'User registration form', type: :feature do
       click_on 'Create User'
 
       expect(page).to have_content "Welcome, #{username}!"
+    end
+
+    it "can't create a new user due to mismatch password and password confirmation" do
+      Campaign.create(name: "Turing West Marches", status: "active")
+
+      visit root_path
+
+      click_on "Register"
+
+      expect(current_path).to eq "/users/new"
+
+      username = "BurtReynolds"
+      password = "hamburger1"
+      bad_password = "hotdog"
+      first_name = "Burt"
+      last_name = "Reynolds"
+      email = "burtreynolds@example.com"
+
+      fill_in :username, with: username
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: bad_password
+      fill_in :first_name, with: first_name
+      fill_in :last_name, with: last_name
+      fill_in :email, with: email
+
+      click_on "Create User"
+
+      expect(page).to have_content "Password confirmation doesn't match Password"
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,23 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
+
+    describe "password confirmation" do
+      it "validates that password and password confirmation are the same" do
+        password = "sword"
+        user = build(:user, password: password, password_confirmation: password)
+
+        expect(user).to be_valid
+      end
+
+      it "invalidates that password and password confirmation are different" do
+        password = "sword"
+        password_confirmation = "shield"
+        user = build(:user, password: password, password_confirmation: password_confirmation)
+
+        expect(user).not_to be_valid
+      end
+    end
   end
 
   describe 'roles' do
