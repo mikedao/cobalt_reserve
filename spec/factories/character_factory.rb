efe03234fb7e6e97e4c64bcbad78939eb6e04d17
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-07-30 15:02:11 -0600

    Refactor species and class to sample from actual options

--- spec/factories/character_factory.rb
+++ spec/factories/character_factory.rb
@@ -3,9 +3,9 @@ require 'faker'
 FactoryBot.define do
   factory :character do
     name { Faker::Games::WorldOfWarcraft.unique.hero }
-    species { "Humanoid (#{Faker::Games::DnD.species})" }
+    species { Character.species.drop(1).sample }
     level { Faker::Number.between(from: 1, to: 19).to_i+1 }
-    character_class { "#{Faker::Games::DnD.klass} Class" }
+    character_class { Character.classes.drop(1).sample }
     campaign
     user
     dndbeyond_url { "http://dndbeyond/#{Faker::Number.within(range: 100000..999999)}"}
