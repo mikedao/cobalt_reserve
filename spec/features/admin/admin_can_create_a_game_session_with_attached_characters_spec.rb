4c10e8f415f93933b74cf3fa5429aebfcf60614c
Michael Dao <mike@turing.io>
2020-07-30 18:43:39 -0600

    ensures non active characters do not appear on the new game session form

--- spec/features/admin/admin_can_create_a_game_session_with_attached_characters_spec.rb
+++ spec/features/admin/admin_can_create_a_game_session_with_attached_characters_spec.rb
@@ -28,6 +28,24 @@ RSpec.describe 'admin dashboard index', type: :feature do
         expect(page).to_not have_content(char2.name)
         expect(page).to have_content(Date.today)
       end
+
+      it 'does not let me add an inactive character to a session' do
+        campaign = create(:campaign)
+        char1 = create(:character, campaign: campaign)
+        char2 = create(:character, campaign: campaign)
+        char3 = create(:character, campaign: campaign)
+        char4 = create(:character, campaign: campaign, active: false)
+        user = create(:admin_user)
+
+        login_as_user(user.username, user.password)
+        visit admin_dashboard_path
+
+        click_on 'Create Session'
+
+        expect(current_path).to eq(admin_game_sessions_new_path)
+
+        expect(page).to_not have_content(char4.name)
+      end
     end
   end
 end
