f730c62a243aafcc69a1b197b8f063cd048f01bf
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-08-01 12:12:38 -0600

    Add helper method for current_admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user,
-                :current_campaign
+                :current_campaign,
+                :current_admin?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
