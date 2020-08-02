e7d50a272c5e6b5621e66bde6087ea3cee8a8318
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-08-01 21:03:17 -0600

    Change model tests to expect adventure log to belong to character

--- spec/models/adventure_log_spec.rb
+++ spec/models/adventure_log_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe AdventureLog, type: :model do
   describe 'relationships' do
     it { should belong_to :game_session }
-    it { should belong_to :user }
+    it { should belong_to :character }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many :characters }
-    it { should have_many :adventure_logs }
   end
 
   describe 'validations' do
