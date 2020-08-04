9d4cac6fe9537513ddd2e75b5adb3f28b96dc3bb
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-08-03 21:44:31 -0600

    Resolve rubocop issues

--- spec/features/game_sessions/anyone_can_see_game_session_index_spec.rb
+++ spec/features/game_sessions/anyone_can_see_game_session_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "game session index", type: :feature do
+RSpec.describe 'game session index', type: :feature do
   before do
     campaign = create(:campaign)
     @game_session_1 = create(:game_session, campaign: campaign)
@@ -10,7 +10,11 @@ RSpec.describe "game session index", type: :feature do
 
   context 'as a visitor' do
     it 'I see a link on the homepage which takes me to game session index' do
-      validate_game_session_index
+      validate_game_session_link
+
+      expect(page).to have_link @game_session_1.name, href: game_session_path(@game_session_1)
+      expect(page).to have_link @game_session_2.name, href: game_session_path(@game_session_2)
+      expect(page).to have_link @game_session_3.name, href: game_session_path(@game_session_3)
     end
 
     it 'I can click the name of a game session to go to its show page' do
@@ -25,24 +29,21 @@ RSpec.describe "game session index", type: :feature do
 
   context 'as a logged-in standard user' do
     it 'I see a link on the homepage which takes me to game session index' do
-      validate_game_session_index
+      validate_game_session_link
     end
   end
 
   context 'as a logged-in admin user' do
     it 'I see a link on the homepage which takes me to game session index' do
-      validate_game_session_index
+      validate_game_session_link
     end
   end
 end
 
-def validate_game_session_index
+def validate_game_session_link
   visit '/'
 
   click_link 'Game Session Index'
 
   expect(current_path).to eq '/game_sessions'
-  expect(page).to have_link @game_session_1.name, href: game_session_path(@game_session_1)
-  expect(page).to have_link @game_session_2.name, href: game_session_path(@game_session_2)
-  expect(page).to have_link @game_session_3.name, href: game_session_path(@game_session_3)
 end
