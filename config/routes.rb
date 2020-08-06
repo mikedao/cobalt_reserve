9698e3654335bc26691d65740005a5d0501740a9
Michael Dao <mike@turing.io>
2020-08-05 21:06:52 -0600

    Adds foundry key management for admins

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     get '/dashboard', to: 'dashboard#show'
     get '/game_sessions/new', to: 'game_sessions#new'
     post '/game_sessions', to: 'game_sessions#create'
+    get '/foundry_key', to:  'foundry_key#index'
+    get '/foundry_key/:id/edit', to: 'foundry_key#edit', as: :edit_foundry_key
+    patch '/foundry_key/:id', to: 'foundry_key#update', as: :update_foundry_key
   end
 
   get '/passwordless-login', to: 'sessions#passwordless_new', as: :passwordless_login
@@ -20,7 +23,6 @@ Rails.application.routes.draw do
 
   get '/characters/:id/backstory/edit', to: 'backstory#edit', as: :backstory_edit
   patch '/characters/:id/backstory', to: 'backstory#update', as: :backstory_update
-  patch '/characters/:id/foundry_key', to: 'foundry_key#update', as: :foundry_key_update
 
   resources :game_sessions, only: %i[index show] do
     resources :adventure_logs, only: %i[new create]
