ccb4c71441e23e7130f52d9e49ac6eb3d9593b42
Michael Dao <mike@turing.io>
2020-08-06 17:55:12 -0600

    ran down a bad typo i made and fixes some hound stuff

--- app/models/world_news.rb
+++ app/models/world_news.rb
@@ -1,6 +1,5 @@
 class WorldNews < ApplicationRecord
   belongs_to :campaign
 
-  scope :most_recently_created, -> { order("created_at").last }
-
+  scope :most_recently_created, -> { order('created_at').last }
 end
