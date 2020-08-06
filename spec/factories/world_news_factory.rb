985dbee1a11557410f7c61f152acdce4f7c9b413
Michael Dao <mike@turing.io>
2020-08-05 22:35:14 -0600

    World news now displays on root page

--- spec/factories/world_news_factory.rb
+++ spec/factories/world_news_factory.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :world_news do
     date { Faker::Business.credit_card_expiry_date }
     title { Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote }
-    body { Faker::Movies::HitchhikersGuideToTheGalaxy. }
+    body { Faker::Movies::HitchhikersGuideToTheGalaxy.quote }
     campaign
   end
 end
