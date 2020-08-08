7c2c93de48b6b06c881e30914b5a23b4577ab229
Michael Dao <mike@turing.io>
2020-08-08 12:57:05 -0600

    high res link added to home page

--- spec/factories/world_map_factory.rb
+++ spec/factories/world_map_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :world_map do
     low_res { "https://i.imgur.com/rBxkv0D.jpg" }
-    high_res { "https://i.imgur.com/JTXgJPB.jpg" }
+    high_res { "https://i.imgur.com/zPIS6Ig.jpg" }
     campaign
   end
 end
--- spec/features/world_map/world_map_visibility_spec.rb
+++ spec/features/world_map/world_map_visibility_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe 'low res world map appears on home page', type: :feature do
         expect(page).to have_css("img[src*='rBxkv0D.jpg']")
       end
     end
+
+    it 'has a link to the high res version on the home page' do
+      campaign = create(:campaign)
+      world_map = create(:world_map, campaign: campaign)
+      visit root_path
+
+      save_and_open_page
+      within('#world-map') do
+        expect(page).to have_link('High Res Version', href: 'https://i.imgur.com/zPIS6Ig.jpg')
+      end
+    end
   end
 end
