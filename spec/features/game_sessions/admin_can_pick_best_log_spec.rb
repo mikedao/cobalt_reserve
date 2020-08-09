d5b092e5e20d261ab55e1ba6e2a314fb250ae148
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-08-09 13:00:05 -0600

    Refactor test with section ID helper methods

--- spec/features/game_sessions/admin_can_pick_best_log_spec.rb
+++ spec/features/game_sessions/admin_can_pick_best_log_spec.rb
@@ -3,17 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Best Adventure Log', type: :feature do
   before(:context) do
     @campaign = create(:campaign)
-    @game_session = GameSession.create(name:      "Shelob's Lair",
-                                       campaign:  @campaign)
+    @game_session = create(:game_session, campaign:  @campaign)
     @character_1 = create(:character)
     @character_2 = create(:character)
     @game_session.characters << [@character_1, @character_2]
-    @log_1 = create(:adventure_log, character: @character_1, game_session: @game_session)
-    @log_2 = create(:adventure_log, character: @character_2, game_session: @game_session)
-    @log_3 = create(:adventure_log, character: @character_1, game_session: @game_session)
-    LOG_1_SECTION = "adventure-log-#{@log_1.id}"
-    LOG_2_SECTION = "adventure-log-#{@log_2.id}"
-    LOG_3_SECTION = "adventure-log-#{@log_3.id}"
+    @log_1 = create(:adventure_log, character: @character_1, game_session: @game_session, created_at: Date.today)
+    @log_2 = create(:adventure_log, character: @character_2, game_session: @game_session, created_at: Date.today + 1)
+    @log_3 = create(:adventure_log, character: @character_1, game_session: @game_session, created_at: Date.today + 2)
   end
 
   context 'as a logged-in admin' do
@@ -32,8 +28,8 @@ RSpec.describe 'Best Adventure Log', type: :feature do
       end
 
       it 'I see the logs ordered chronologically by creation' do
-        expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_2_SECTION)
-        expect(page.body.index(LOG_2_SECTION)).to be < page.body.index(LOG_3_SECTION)
+        expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_2))
+        expect(page.body.index(section_name(@log_2))).to be < page.body.index(section_name(@log_3))
       end
 
       it 'I see a button next to each adventure log to mark it best' do
@@ -46,9 +42,7 @@ RSpec.describe 'Best Adventure Log', type: :feature do
 
       context 'when I click a Mark as Best button' do
         before do
-          within "#adventure-log-#{@log_2.id}" do
-            click_on 'Mark as Best'
-          end
+          find(section_id(@log_2)).click_on 'Mark as Best'
         end
 
         it 'I remain on the game session show page' do
@@ -57,16 +51,16 @@ RSpec.describe 'Best Adventure Log', type: :feature do
 
         it 'I can see the best adventure log moved to top spot with a logo' do
           expect(page).to have_content @log_2.content, count: 1
-          expect(page.body.index(LOG_2_SECTION)).to be < page.body.index(LOG_1_SECTION)
-          expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_3_SECTION)
+          expect(page.body.index(section_name(@log_2))).to be < page.body.index(section_name(@log_1))
+          expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_3))
           within '#best-adventure-log' do
-            expect(page).to have_selector "#adventure-log-#{@log_2.id}"
-            expect(page).to have_selector 'img', alt: 'Medal Symbol'
+            expect(page).to have_selector section_id(@log_2)
+            expect(page).to have_selector 'img[@alt="Medal Symbol"]'
           end
         end
 
         it 'I see the button text next to the best log changed' do
-          within '#' + LOG_2_SECTION do
+          within section_id(@log_2) do
             expect(page).to have_button 'Unmark as Best'
             expect(page).not_to have_button 'Mark as Best'
           end
@@ -86,12 +80,12 @@ RSpec.describe 'Best Adventure Log', type: :feature do
           end
 
           it 'the adventure logs return to being ordered chronologically' do
-            expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_2_SECTION)
-            expect(page.body.index(LOG_2_SECTION)).to be < page.body.index(LOG_3_SECTION)
+            expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_2))
+            expect(page.body.index(section_name(@log_2))).to be < page.body.index(section_name(@log_3))
           end
           
           it 'the button text changes back to mark as best' do
-            within '#' + LOG_2_SECTION do
+            within section_id(@log_2) do
               expect(page).not_to have_button 'Unmark as Best'
               expect(page).to have_button 'Mark as Best'
             end
@@ -100,21 +94,19 @@ RSpec.describe 'Best Adventure Log', type: :feature do
 
         context 'when I click the button next to another log' do
           before do
-            within '#' + LOG_3_SECTION do
-              click_on 'Mark as Best'
-            end
+            find(section_id(@log_3)).click_on 'Mark as Best'
           end
 
           it 'that log is now in the best log spot' do
             within '#best-adventure-log' do
-              expect(page).to have_selector LOG_3_SECTION
-              expect(page).not_to have_selector LOG_2_SECTION
+              expect(page).to have_selector section_id(@log_3)
+              expect(page).not_to have_selector section_id(@log_2)
             end
           end
 
-          it 'the previous best log is back in reverse chronological order' do
-            expect(page.body.index(LOG_3_SECTION)).to be < page.body.index(LOG_1_SECTION)
-            expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_2_SECTION)
+          it 'the previous best log is back in chronological order' do
+            expect(page.body.index(section_name(@log_3))).to be < page.body.index(section_name(@log_1))
+            expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_2))
           end
         end
       end
@@ -129,7 +121,7 @@ RSpec.describe 'Best Adventure Log', type: :feature do
 
     context 'when I go to a game session show page with a best log marked' do
       before do
-        @log_2.best = true
+        AdventureLog.find(@log_2.id).update(best: true)
         visit game_session_path(@game_session)
       end
 
@@ -138,11 +130,11 @@ RSpec.describe 'Best Adventure Log', type: :feature do
       end
 
       it 'I can see the best adventure log displayed first with a logo' do
-        expect(page.body.index(LOG_2_SECTION)).to be < page.body.index(LOG_1_SECTION)
-        expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_3_SECTION)
+        expect(page.body.index(section_name(@log_2))).to be < page.body.index(section_name(@log_1))
+        expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_3))
         within '#best-adventure-log' do
-          expect(page).to have_selector '#' + LOG_2_SECTION
-          expect(page).to have_selector 'img', alt: 'Medal Symbol'
+          expect(page).to have_selector section_id(@log_2)
+          expect(page).to have_selector 'img[@alt="Medal Symbol"]'
         end
       end
     end
@@ -151,7 +143,7 @@ RSpec.describe 'Best Adventure Log', type: :feature do
   context 'as a visitor' do
     context 'when I go to a game session show page with a best log marked' do
       before do
-        @log_2.best = true
+        AdventureLog.find(@log_2.id).update!(best: true)
         visit game_session_path(@game_session)
       end
 
@@ -160,13 +152,21 @@ RSpec.describe 'Best Adventure Log', type: :feature do
       end
 
       it 'I can see the best adventure log displayed first with a logo' do
-        expect(page.body.index(LOG_2_SECTION)).to be < page.body.index(LOG_1_SECTION)
-        expect(page.body.index(LOG_1_SECTION)).to be < page.body.index(LOG_3_SECTION)
+        expect(page.body.index(section_name(@log_2))).to be < page.body.index(section_name(@log_1))
+        expect(page.body.index(section_name(@log_1))).to be < page.body.index(section_name(@log_3))
         within '#best-adventure-log' do
-          expect(page).to have_selector '#' + LOG_2_SECTION
-          expect(page).to have_selector 'img', alt: 'Medal Symbol'
+          expect(page).to have_selector section_id(@log_2)
+          expect(page).to have_selector 'img[@alt="Medal Symbol"]'
         end
       end
     end
   end
+end
+
+def section_name(log)
+  "adventure-log-#{log.id}"
+end
+
+def section_id(log)
+  '#' + section_name(log)
 end
