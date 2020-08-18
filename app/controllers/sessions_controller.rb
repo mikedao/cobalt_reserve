4f2b9cfa1eded3d225da07fe08e55e954d44ff8e
Michael Dao <mike@turing.io>
2020-08-17 20:04:41 -0600

    Fixes it so that inactive users can no longer log in with a regular password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    if user&.authenticate(params[:password])
+    if user&.authenticate(params[:password]) && user.active?
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.username}!"
       if user.admin?
